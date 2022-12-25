b7ec605bb225b010e0647d68574d1b6febe86178
Nisal Vimukthi <50359663+Neeka0909@users.noreply.github.com>
2022-12-25 15:50:08 +0530

    EJS template with NodeJs

---
+++ 13 Getting started with EJS Templates with Express/Readme.md
@@ -0,0 +1,3 @@
+# This tutorial inbcludes the following sections:
+## 1. Getting Stratred with EJS Templates with Express
+## 2. HTTP POST Request W/ Express and Body Parser Module
---
+++ 13 Getting started with EJS Templates with Express/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+//app.use is used to add middleware to the request processing pipeline
+app.use('/public', express.static(path.join(__dirname, 'static')));  // This is the default path 
+app.set('view engine', 'ejs');
+
+app.get('/:userQuery/:queryData', (req, res) => {
+    res.render('index', { data: { userQuery: req.params.userQuery, queryData: req.params.queryData, searhResults:['book1', 'book2'] } });
+});
+
+
+
+app.listen(3000, () => {
+    console.log("Web Serer Started");
+});
